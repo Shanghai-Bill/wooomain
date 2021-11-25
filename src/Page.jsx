66c53818e2b8d9cfeb79d10322a4e38f16c443d5
UnWeird <zhengjunhao@corp.netease.com>
2021-11-26 05:01:30 +0800

    feat: 增加friends模块，调整ui

--- src/Page.jsx
+++ src/Page.jsx
@@ -4,6 +4,7 @@ import Staking from "./components/Staking"
 // import { useQuery } from '@apollo/client'
 import { parseGraphObject, QUERY } from './utils/query'
 import WoodButton from './components/WoodButton'
+import { useEffect, useState } from 'react'
 
 const Page = ({wallet, chain, wool, reload, total, woolf, stakedWoolf}) => {
 
@@ -19,6 +20,24 @@ const Page = ({wallet, chain, wool, reload, total, woolf, stakedWoolf}) => {
     tokens = [...data.tokens.map(x => parseGraphObject(x))].sort((e1, e2) => {return parseInt(e1.id, 16) - parseInt(e2.id, 16)}) 
     staked = [...data.stakes.map(x => parseGraphObject(x))].sort((e1, e2) => {return parseInt(e1.id, 16) - parseInt(e2.id, 16)})
   }
+
+  const friendWords = ['list', '', 'invite me', 'I invite']
+  const friendList = ['0x000000000000001','0x000000000000002','0x000000000000003','0x000000000000004'];
+  const inviteList = ['0x100000000000000','0x110000000000000','0x400000000000000','0x500000000000000'];
+  const invitedList = ['0x200000000000000','0x220000000000000','0x300000000000000','0x600000000000000'];
+  const [inputId, setInputId] = useState('');
+  const [showList, setShowList] = useState(friendList);
+  const [selectedList, setSelectedList] = useState([0,0,0,0]);
+  const [showType, setShowType] = useState(0);
+
+
+  useEffect(()=>{
+    //TODO：根据点击的showType更新showList
+    setSelectedList(showList.map(() => false))
+  }, [showType])
+
+
+
 
   return (
     <div className="w-full flex flex-col md:justify-center items-center p-5">
@@ -57,6 +76,71 @@ const Page = ({wallet, chain, wool, reload, total, woolf, stakedWoolf}) => {
 
       
       <div className="flex flex-col md:flex-row justify-center items-center gap-10 w-full" style={{maxWidth:'1500px'}}>
+        <div className="h-full w-full md:w-1/2 flex justify-between flex flex-col gap-5">
+          <Container>
+          <div className="flex flex-col items-center font-pixel gap-5" >
+              <div className={showType==0 ? 'selected' : ''}><WoodButton width={250} height={50} title={'friend list'} fontSize={15} onClick={() => {
+                setShowType(0)
+              }} /></div>
+              <div className={showType==1 ? 'selected' : ''}><WoodButton width={250} height={50} title={'add friends'} fontSize={15} onClick={() => {
+                setShowType(1)
+              }} /></div>
+              <div className={showType==2 ? 'selected' : ''}><WoodButton width={250} height={50} title={'friends invite me'} fontSize={15} onClick={() => {
+                setShowType(2)
+              }} /></div>
+              <div className={showType==3 ? 'selected' : ''}><WoodButton width={250} height={50} title={'friends I invite'} fontSize={15} onClick={() => {
+                setShowType(3)
+              }} /></div>
+            </div>
+          </Container>
+
+          <Container>
+            <div className="flex flex-col items-center font-pixel gap-5" style={{'overflow-y': 'scroll', maxHeight: '450px'}}>
+              <div className="subtitle mt-5">
+                friends
+              </div>
+              <div className="subtitle">
+                {friendWords[showType]}
+              </div>
+              {showType == 1
+                ? (<input onChange={(e) => {
+                  setInputId(e.target.value)
+                }}></input>)
+                : showList.map((str, index) => (<div key={str+index} className={selectedList[index] ? 'selected class-for-ids' : 'class-for-ids'}><WoodButton width={250} height={50} title={str} fontSize={15} onClick={(event) => {
+                  if(showType == 0) {
+                    navigator.clipboard.writeText(str).then(()=>{
+                      alert("Friend's address copied!")
+                    });
+                    
+                  } else {
+                    selectedList[index] = !selectedList[index]
+                    setSelectedList([...selectedList])
+                  }
+              }} /></div>))}
+              {showType == 1 && (<WoodButton width={150} height={75} title={'add'} fontSize={15} onClick={() => {
+                console.log(inputId);
+                //添加朋友
+              }} />)}
+              {showType == 2 && (
+                <div className="flex flex-col md:flex-row justify-center items-center gap-5 mb-2">
+                  <WoodButton width={100} height={80} title={'approve'} fontSize={15} onClick={() => {
+                  //同意请求
+                  console.log(showList.filter((obj, idx) => selectedList[idx]))
+                }} />
+                  <WoodButton width={100} height={80} title={'reject'} fontSize={15} onClick={() => {
+                  //拒绝请求
+                  console.log(showList.filter((obj, idx) => selectedList[idx]))
+                }} />
+                </div>
+                
+              )}
+              {showType == 3 && (<WoodButton width={150} height={75} title={'cancel'} fontSize={15} onClick={() => {
+                console.log(showList.filter((obj, idx) => selectedList[idx]))
+                //撤回申请
+              }} />)}
+            </div>
+          </Container>
+        </div>
         <div className="h-full w-full md:w-1/2 flex justify-between flex flex-col gap-5">
           <Minting wallet={wallet} chain={chain} total={total} reload={() => {
             reload()
